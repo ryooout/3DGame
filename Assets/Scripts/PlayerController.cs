be18fe597644bc40962acc4f6ee603a1aaeff46e
ryooout <ryota01010104@gmail.com>
2022-07-28 16:45:31 +0900

    TPS視点のカメラを作った

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     Animator animator;
-    [SerializeField] float speed = 10.0f;
-    private Vector3 latestPos;
+    Quaternion targetRot;
+    private void Awake()
+    {
+        targetRot = transform.rotation;
+    }
     void Start()
     {
+        TryGetComponent(out animator);
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -19,61 +22,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector3(x, 0, z);
-        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
-        latestPos = transform.position;  //前回のPositionの更新
-        //ベクトルの大きさが0.01以上の時に向きを変える処理をする
-        if (diff.magnitude > 0.01f)
-        {
-            transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //カメラの正面方向に移動するための変数。
+        var horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        var velocity =horizontalRotation* new Vector3(x, 0, z).normalized;
+        var speed = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+        var rotationSpeed = 600 * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
-        else if(Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(0))
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-        if (Mathf.Abs(x) > 0 || Mathf.Abs(z) > 0)
+        //移動方向を向く
+        if(velocity.magnitude>0.5f)
         {
-            //falseのとき
-            if (!animator.GetBool("Walk"))
-            {
-                //walkをtrueに変更する処理
-                animator.SetBool("Walk", true);
-            }
-        }
-        //trueのとき
-        else if (animator.GetBool("Walk"))
-        {
-            animator.SetBool("Walk", false);
-        }
-
-        //前に動いているとき
-        if (z > 0 && Input.GetKey(KeyCode.LeftShift))
-        {
-            //falseのとき
-            if (!animator.GetBool("Run"))
-            {
-                //walkをtrueに変更する処理
-                animator.SetBool("Run", true);
-                speed = 12.5f;
-                Debug.Log(speed);
-            }
-        }
-        //trueのとき
-        else if (animator.GetBool("Run"))
-        {
-            animator.SetBool("Run", false);
-            speed = 10.0f;
-            Debug.Log(speed);
-        }
-
+            targetRot = Quaternion.LookRotation(velocity, Vector3.up);
+        }                                //回転をなめらかに
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotationSpeed);
+        animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
     }
     private void FixedUpdate()
     {
