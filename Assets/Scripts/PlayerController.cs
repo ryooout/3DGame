82db78aa8882af91c6e8c7c018ab7f9823786511
ryooout <ryota01010104@gmail.com>
2022-08-01 11:31:07 +0900

    関数を新しく作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         TryGetComponent(out animator);
-        rb = GetComponent<Rigidbody>();
+        TryGetComponent(out rb);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.None;
     }
@@ -29,20 +29,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            //ƒJƒƒ‰‚Ì³–Ê•ûŒü‚ÉˆÚ“®‚·‚é‚½‚ß‚Ì•Ï”B
-            var horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
-            var velocity = horizontalRotation * new Vector3(x, 0, z).normalized;
-            var speed = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
-            var rotationSpeed = 600 * Time.deltaTime;
-            //ˆÚ“®•ûŒü‚ðŒü‚­
-            if (velocity.magnitude > 0.5f)
-            {
-                targetRot = Quaternion.LookRotation(velocity, Vector3.up);
-            }                                //‰ñ“]‚ð‚È‚ß‚ç‚©‚É
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotationSpeed);
-            animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
+        Move();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.visible = true;
@@ -57,8 +44,25 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
     }
-    public void AttackButton()
+    private void Move()
     {
-        animator.SetTrigger("Attack");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //ƒJƒƒ‰‚Ì³–Ê•ûŒü‚ÉˆÚ“®‚·‚é‚½‚ß‚Ì•Ï”B
+        var horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        var velocity = horizontalRotation * new Vector3(x, 0, z).normalized;
+        var speed = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+        var rotationSpeed = 600 * Time.deltaTime;
+        //ˆÚ“®•ûŒü‚ðŒü‚­
+        if (velocity.magnitude > 0.5f)
+        {
+            targetRot = Quaternion.LookRotation(velocity, Vector3.up);
+        }                                //‰ñ“]‚ð‚È‚ß‚ç‚©‚É
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotationSpeed);
+        animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+        }
     }
 }
