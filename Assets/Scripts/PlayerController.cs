962bf307bdcb40ccb2e4edde9df05b0d6a5d4d19
ryooout <ryota01010104@gmail.com>
2022-09-04 23:15:01 +0900

    playerのアニメーション遷移と、スピード調整

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,17 +10,22 @@ public class PlayerController : MonoBehaviour
     Quaternion targetRot;
     public Collider[] Hitcollider;
     /// <summary>体力</summary>
-    public int playerHp = 100;
+    [SerializeField]private int playerHp = 100;
     /// <summary>Maxの体力</summary>
-    int maxPlayerHp = 100;
+    [SerializeField]private int maxPlayerHp = 100;
     /// <summary>hpバー </summary>
     public Slider hpBer;
     float currentTime = 0;
     public static bool heal_flag = false;
     [SerializeField] Button healButton = default;
     [SerializeField] GameObject healObj;
+    [SerializeField]float playerSpeed = 5;
     float jumpForce = 200f;
     bool isGround = true;
+
+    UiController uiContoroller;
+    SaveManager saveManager;
+    Vector3 vel;
     private void Awake()
     {
         targetRot = transform.rotation;
@@ -28,14 +33,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+
         rb = GetComponent<Rigidbody>();
+        /*saveManager = GetComponent<SaveManager>();
+        playerHp = saveManager.PlayerHp;*/
         Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             Cursor.visible = true;
@@ -55,7 +63,7 @@ public class PlayerController : MonoBehaviour
                 rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
             }
         }*/
-        if (UiContoroller.enemyCount == 0)
+        if (UiController.uiController.enemyCount == 0)
         {
             rb.isKinematic = true;
         }
@@ -86,23 +94,38 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        Move();
     }
     private void Move()
     {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            return;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         //カメラの正面方向に移動するための変数。
         var horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
-        var velocity = horizontalRotation * new Vector3(x, 0, z).normalized;
+        vel = horizontalRotation * new Vector3(x, 0, z);
+        vel.Normalize();
+        rb.velocity = vel*playerSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = 4;
+        }
+        else
+        {
+            playerSpeed = 2;
+        }       
         var speed = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
         var rotationSpeed = 600 * Time.deltaTime;
         //移動方向を向く
-        if (velocity.magnitude > 0.5f)
+        if (vel.magnitude > 0.5f)
         {
-            targetRot = Quaternion.LookRotation(velocity, Vector3.up);
+            targetRot = Quaternion.LookRotation(vel, Vector3.up);
         }                                //回転をなめらかに
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotationSpeed);
-        animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
+        animator.SetFloat("Speed", vel.magnitude * speed, 0.1f, Time.deltaTime);
         if (Input.GetMouseButtonDown(0))
         {
             if (EventSystem.current.IsPointerOverGameObject())
@@ -111,7 +134,8 @@ public class PlayerController : MonoBehaviour
                 return;
             }
             animator.SetTrigger("Attack");
-        } 
+            
+        }
     }
     public void HideColliderWeapon()
     {
