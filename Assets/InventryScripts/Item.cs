38878fada42e14c19c4390087c9445e9e7eb5eea
ryooout <ryota01010104@gmail.com>
2022-09-06 00:10:47 +0900

    enumで属性を追加

---
+++ Assets/InventryScripts/Item.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+//unityEditorÇ©ÇÁåƒÇ—èoÇπÇÈÇÊÇ§Ç…ÇµÇΩ
+[CreateAssetMenu(fileName ="New Item",menuName = "ScriptableObject/Create Item")]
+public class Item : ScriptableObject
+{
+    public Item.ItemId Id = default;
+    public static Item itemInstance;
+    new public string name = "New Item";
+    public Sprite itemIcon = null;
+    public enum ItemId
+    {
+        None,
+        Atk,
+        Def,
+        Heal,
+    }
+    public string ItemName()
+    {
+        return name;
+    }
+
+}
