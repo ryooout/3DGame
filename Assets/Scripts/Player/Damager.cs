40101f0690b535e9cd0608132dd26706897c6dd6
ryooout <ryota01010104@gmail.com>
2022-09-16 23:30:22 +0900

    ダメージクラス

---
+++ Assets/Scripts/Player/Damager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [SerializeField]private float damage;
+    public float Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
